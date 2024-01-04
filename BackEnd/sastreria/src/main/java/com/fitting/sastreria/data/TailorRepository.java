a73dae12f2a43f5bc632b70fb2fb810e3a3be114
javier2872 <javier2872@hotmail.com>
2024-01-04 02:27:11 +0100

    added tested of controller

--- BackEnd/sastreria/src/main/java/com/fitting/sastreria/data/TailorRepository.java
+++ BackEnd/sastreria/src/main/java/com/fitting/sastreria/data/TailorRepository.java
@@ -6,23 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fitting.sastreria.model.pojo.Tailor;
 
+
 /**
  * searches in the jpaRepository
  */
 
 public interface TailorRepository extends JpaRepository<Tailor, Long> {
-
+		
 	Long deleteById(long id);
-
-	List<Tailor> findByTypeService(String typeService);
-
+	
+	Tailor getReferenceById(Long id);
+		
 	List<Tailor> findByName(String name);
-
-	List<Tailor> findByPrice(String price);
-
-	List<Tailor> findByOpinion(String opinion);
-
+	
 	List<Tailor> findBySpecialtiesName(String name);
-
-	List<Tailor> findAllByAvailabilityAvailability(String availability);
+	
+	List<Tailor> findAllByAvailability(String availability);
 }
--- BackEnd/sastreria/src/main/java/com/fitting/sastreria/utils/Specialties.java
+++ BackEnd/sastreria/src/main/java/com/fitting/sastreria/utils/Specialties.java
@@ -1,16 +1,6 @@
 package com.fitting.sastreria.utils;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fitting.sastreria.model.pojo.Tailor;
+import javax.persistence.Embeddable;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,32 +10,13 @@ import lombok.Setter;
 /**
  * Specialties of a tailor service
  */
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "specialties")
+@Embeddable
 public class Specialties {
-
-	public Specialties(String nameArg, String priceArg) {
-		this.name = nameArg;
-		this.price = priceArg;
-	}
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-
-	@ManyToOne
-	@JsonBackReference
-	@JoinColumn(name = "tailorId")
-	private Tailor tailorId;
-
-	@Column(name = "name")
 	private String name;
-
-	@Column(name = "price")
 	private String price;
 
 }
